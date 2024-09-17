2340126faeee22747a8cbaeb38263a8f33fa7106
Deepak Gola <rudra.pratap91@live.com>
2024-09-17 22:53:55 +0530

    Added Vector Stores for RAG (Retrieval-Augmented Generation)

---
+++ src/main/java/com/deepak/springai/springairag/config/VectorStoreConfig.java
@@ -0,0 +1,42 @@
+package com.deepak.springai.springairag.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class VectorStoreConfig {
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel, VectorStoreProperties vectorStoreProperties) {
+        SimpleVectorStore store = new SimpleVectorStore(embeddingModel);
+        File vectorStoreFile = new File(vectorStoreProperties.getVectorStorePath());
+        if(vectorStoreFile.exists()){
+            store.load(vectorStoreFile);
+        } else {
+            log.debug("Loading documents into vector store");
+            vectorStoreProperties.getDocumentsToLoad().forEach((document)-> {
+                System.out.println("Loading document: "+document.getFilename());
+
+                TikaDocumentReader documentReader = new TikaDocumentReader(document);
+                List<Document> docs = documentReader.get();
+                TextSplitter textSplitter = new TokenTextSplitter();
+                List<Document> splitDocs = textSplitter.apply(docs);
+                store.add(splitDocs);
+
+            });
+            store.save(vectorStoreFile);
+        }
+        return store;
+    }
+}
